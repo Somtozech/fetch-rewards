1ec7efaed88377b1e41c0da39978e598cf60880f
Somtozech <ezechinnaemeka@gmail.com>
2022-05-03 10:03:48 +0100

    feat: setup Error Exception filter to handle errors centrally

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,22 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './utils/error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
 
-  app.useGlobalPipes(new ValidationPipe());
-
   app.enableShutdownHooks();
   app.setGlobalPrefix(configService.get('app.apiPrefix'), {
     exclude: ['/'],
   });
-
   app.enableVersioning({
     type: VersioningType.URI,
   });
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   const config = new DocumentBuilder()
     .setTitle('Fetch Rewards Backend Task')
---
+++ src/utils/error.filter.ts
@@ -0,0 +1,37 @@
+import { AxiosError } from 'axios';
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    if (exception instanceof HttpException) {
+      const status = exception.getStatus();
+      const errorResponse = exception.getResponse() as {
+        message: string[] | string;
+      };
+      return response.status(status).json({
+        statusCode: status,
+        message: Array.isArray(errorResponse.message)
+          ? errorResponse.message[0]
+          : errorResponse.message,
+        timestamp: new Date().toISOString(),
+      });
+    }
+
+    return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+      status: HttpStatus.INTERNAL_SERVER_ERROR,
+      message: 'Internal Server Error',
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
